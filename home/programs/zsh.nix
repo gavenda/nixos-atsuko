a159a07e79f5bdb58165e343e50d9a34ad4ff4a4
Enda <gavenda@disroot.org>
2024-04-17 02:01:21 +0800

    Add missing ./zsh.nix import

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -3,10 +3,6 @@
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
