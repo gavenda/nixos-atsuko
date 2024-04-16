2c540af33fa6d8f2e2233e1c4fa4bcf7c467e99f
Enda <gavenda@disroot.org>
2024-04-17 02:34:57 +0800

    Improve font aliasing

--- fonts.nix
+++ fonts.nix
@@ -1,12 +1,36 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      # Monospace
+      (nerdfonts.override { fonts = [ "FiraCode" ]; })
+      fira-code
+      # Noto Sans
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      # Source Code
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      # Liberation
+      liberation_ttf
+      # Microsoft
+      corefonts
+    ];
+
+    fontconfig = {
+      antialias = true;
+      cache32Bit = true;
+      hinting.enable = true;
+      hinting.autohint = true;
+      defaultFonts = {
+        monospace = [ "Source Code Pro" ];
+        sansSerif = [ "Source Sans Pro" ];
+        serif = [ "Source Serif Pro" ];
+      };
+    };
+  };
 }
